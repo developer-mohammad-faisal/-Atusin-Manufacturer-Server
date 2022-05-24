3b0f90763e0a72faa93f67c57ce24b31a36e999b
Your Name <you@example.com>
2022-05-24 23:06:59 +0600

    load all data and update data

--- index.js
+++ index.js
@@ -1,18 +1,100 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 5000
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('This server is Running!')
-})
+app.get("/", (req, res) => {
+  res.send("This server is Running!");
+});
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const res = require("express/lib/response");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p2dpt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("connected to mongodb");
+    const partsCollection = client.db("autusin-Parts").collection("parts");
+    const reviewsCollection = client.db("autusin-Parts").collection("reviews");
+    const userCollection = client.db("autusin-Parts").collection("users");
+
+    // verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const request = req.decoded.email;
+      const requestAccount = await userCollection.findOne({ email: request });
+      if (requestAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "Forbidden" });
+      }
+    };
+
+    // load all parts
+    app.get("/parts", async (req, res) => {
+      const parts = await partsCollection.find({}).toArray();
+      res.send(parts);
+    });
+
+    // load single parts
+    app.get("/parts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // post product
+    app.post("/parts", async (req, res) => {
+      const parts = req.body;
+      const result = await partsCollection.insertOne(parts);
+      res.send(result);
+    });
+
+    // verifyAdmin
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.put("/user/admin/:email", verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // load all reviews
+    app.get("/reviews", async (req, res) => {
+      const reviews = await reviewsCollection.find({}).toArray();
+      res.send(reviews);
+    });
+
+    // post review
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
